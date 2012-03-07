1e3de5cccf1f5e05be53a7662715bda7ea27eda6
ekux44 <ekux44@gmail.com>
2012-03-06 23:54:25 -0600

    Added structure for lose and win screens that are not gameshow themed

--- Mood_Study/src/edu/utulsa/ibcb/moodstudy/GameResultsActivity.java
+++ Mood_Study/src/edu/utulsa/ibcb/moodstudy/GameResultsActivity.java
@@ -6,7 +6,9 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -25,12 +27,28 @@ public class GameResultsActivity extends Activity implements OnClickListener {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+		
+		SharedPreferences settings = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		Boolean gameshow = false;
+			setContentView(R.layout.final_survey);
+		
 		// Load layout from final_survey.xml
 		won = getIntent().getExtras().getBoolean("won");
-		if (won)
-			setContentView(R.layout.gameshow_win);
-		else
-			setContentView(R.layout.gameshow_lose);
+		if (won){
+			if (settings.getString("Theme", "").equals("game_show")) {
+				gameshow = true;
+				setContentView(R.layout.gameshow_win);
+			} else
+				setContentView(R.layout.win);
+		}
+		else{
+			if (settings.getString("Theme", "").equals("game_show")) {
+				gameshow = true;
+				setContentView(R.layout.gameshow_lose);
+			} else
+				setContentView(R.layout.lose);
+		}
 
 		Button replayButton = (Button) findViewById(R.id.replayButton);
 		replayButton.setOnClickListener(this);
