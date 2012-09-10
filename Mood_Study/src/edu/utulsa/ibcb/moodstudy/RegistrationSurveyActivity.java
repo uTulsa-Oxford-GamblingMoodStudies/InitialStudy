e8b2a33b98a5e12e2961143153c19754838ff2a6
ekux44 <ekux44@gmail.com>
2012-09-10 03:14:57 -0500

    Expanded questionnaire

--- Mood_Study/src/edu/utulsa/ibcb/moodstudy/RegistrationSurveyActivity.java
+++ Mood_Study/src/edu/utulsa/ibcb/moodstudy/RegistrationSurveyActivity.java
@@ -14,6 +14,8 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -25,9 +27,12 @@ import android.widget.TextView;
 public class RegistrationSurveyActivity extends Activity implements
 		OnClickListener {
 
-	private int questionNumber = 0;
-	private int[] responses = new int[3];
+	private int questionNumber = 1;
+	private int[] responses;
 	TextView question;
+	RadioGroup radioGroup;
+	private String[] questions;
+	private String[] answer1,answer2,answer3,answer4,answer5,answer6,answer7;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -49,22 +54,67 @@ public class RegistrationSurveyActivity extends Activity implements
 		previousButton.setOnClickListener(this);
 
 		question = (TextView) findViewById(R.id.instructionTextView);
-
+		radioGroup = (RadioGroup)findViewById(R.id.surveyRadioGroup);
+		
+		questions = getResources().getStringArray(R.array.survey_questions);
+		answer1 = getResources().getStringArray(R.array.survey_answer_1);
+		answer2 = getResources().getStringArray(R.array.survey_answer_2);
+		answer3 = getResources().getStringArray(R.array.survey_answer_3);
+		answer4 = getResources().getStringArray(R.array.survey_answer_4);
+		answer5 = getResources().getStringArray(R.array.survey_answer_5);
+		answer6 = getResources().getStringArray(R.array.survey_answer_6);
+		answer7 = getResources().getStringArray(R.array.survey_answer_7);
+		responses = new int[questions.length];
+	
+		loadQuestion();
 	}
 
+	private void loadQuestion(){
+		question.setText(questions[questionNumber-1]);
+		((RadioButton) radioGroup.getChildAt(0)).setText(answer1[questionNumber-1]);
+		((RadioButton) radioGroup.getChildAt(1)).setText(answer2[questionNumber-1]);
+		((RadioButton) radioGroup.getChildAt(2)).setText(answer3[questionNumber-1]);
+		((RadioButton) radioGroup.getChildAt(3)).setText(answer4[questionNumber-1]);
+		((RadioButton) radioGroup.getChildAt(4)).setText(answer5[questionNumber-1]);
+		((RadioButton) radioGroup.getChildAt(5)).setText(answer6[questionNumber-1]);
+		((RadioButton) radioGroup.getChildAt(6)).setText(answer7[questionNumber-1]);
+		
+		
+		
+	    for (int i = 0; i < radioGroup .getChildCount(); i++) {
+	            if(responses[questionNumber-1]==i+1)
+	            	((RadioButton) radioGroup.getChildAt(i)).setChecked(true);
+	            else
+	            	((RadioButton) radioGroup.getChildAt(i)).setChecked(false);
+	        }
+	}
+	
+	private void saveQuestion(){
+		
+	    for (int i = 0; i < radioGroup .getChildCount(); i++) {
+	            if(((RadioButton) radioGroup.getChildAt(i)).isChecked())
+	            	responses[questionNumber-1]=i+1;
+	            	
+	        }
+	}
+	
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.previousButton:
+			saveQuestion();
 			questionNumber--;
-			question.setText("Question " + (questionNumber + 1));
-			if (questionNumber < 0)
+			if (questionNumber < 1)
 				onBackPressed();
+			else
+				loadQuestion();
 			break;
 		case R.id.nextButton:
+			saveQuestion();
 			questionNumber++;
-			question.setText("Question " + (questionNumber + 1));
-			if (questionNumber == responses.length)
+			if (questionNumber > responses.length)
 				onBackPressed();
+			else
+				loadQuestion();
 		}
 
 		/*
